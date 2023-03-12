df52bccd32089be15afa00e28033f018a0563ab4
Eduardo Nogueira <eduardodudunog@gmail.com>
2023-03-12 11:28:36 -0300

    feat: part 1

---
+++ MobileDnr/MobileDnr/MainViewModel.cs
@@ -0,0 +1,97 @@
+﻿using MediaManager;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MobileDnr
+{
+    public class MainViewModel : BaseViewModel
+    {
+        string url = "https://pwop6300.blob.core.windows.net/mtfb/01-Garde1.mp3";
+
+        public MainViewModel()
+        {
+            CrossMediaManager.Current.PositionChanged += Current_PositionChanged;
+            CrossMediaManager.Current.MediaItemFinished += Current_MediaItemFinished;
+        }
+
+        private void Current_PositionChanged(object sender, MediaManager.Playback.PositionChangedEventArgs e)
+        {
+            TimeSpan currentMediaPosition = CrossMediaManager.Current.Position;
+            TimeSpan currentMediaDuration = CrossMediaManager.Current.Duration;
+            TimeSpan timeRemaining = currentMediaDuration.Subtract(currentMediaPosition);
+            if (IsPlaying)
+                CurrentStatus = $"Time Remaining {timeRemaining.Minutes:D2}:{timeRemaining.Seconds:D2}";
+        }
+
+        private void Current_MediaItemFinished(object sender, MediaManager.Media.MediaItemEventArgs e)
+        {
+            IsPlaying = false;
+        }
+
+        private bool isPlaying;
+        public bool IsPlaying
+        {
+            get
+            {
+                return isPlaying;
+            }
+            set
+            {
+                SetProperty(ref isPlaying, value);
+            }
+        }
+
+        private ICommand play;
+
+        public ICommand Play
+        {
+            get
+            {
+                if (play == null)
+                {
+                    play = new AsyncCommand(PerformPlay);
+                }
+
+                return play;
+            }
+        }
+
+        private async Task PerformPlay()
+        {
+            IsPlaying = true;
+            CurrentStatus = "Downloading...";
+            await CrossMediaManager.Current.Play(url);
+        }
+
+        private ICommand stop;
+        public ICommand Stop
+        {
+            get
+            {
+                if (stop == null)
+                {
+                    stop = new AsyncCommand(PerformStop);
+                }
+
+                return stop;
+            }
+        }
+
+        private async Task PerformStop()
+        {
+            IsPlaying = false;
+            CurrentStatus = "";
+            await CrossMediaManager.Current.Stop();
+        }
+
+        private string currentStatus;
+
+        public string CurrentStatus { get => currentStatus; set => SetProperty(ref currentStatus, value); }
+    }
+}
